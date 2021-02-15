3a563a1cb245e43b1e2bb6b0c49549b3b68dae33
Snow White <74522790+snowwhite-boss@users.noreply.github.com>
2021-02-15 12:13:59 +0900

    make role concept

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class User extends Authenticatable
 {
     use Notifiable, HasApiTokens, SoftDeletes;
-    
+
     protected $table = "tbl_users";
 
     /**
@@ -20,7 +20,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'active', 'level', 'access_right'
+        'name', 'email', 'password', 'active', 'level', 'access_right', 'roles'
     ];
 
     /**
@@ -39,9 +39,77 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'roles' => 'array',
     ];
 
-    public function user_accounts() {
+    /**
+     * @return array
+     */
+    public function user_accounts()
+    {
         return $this->hasMany(UserAccounts::class, 'user_id');
+    }
+
+    /**
+     * @param string $role
+     * @return $this
+     */
+    public function addRole(string $role)
+    {
+        $roles = $this->getRoles();
+        $roles[] = $role;
+
+        $roles = array_unique($roles);
+        $this->setRoles($roles);
+
+        return $this;
+    }
+
+    /**
+     * @param array $roles
+     * @return $this
+     */
+    public function setRoles(array $roles)
+    {
+        $this->setAttribute('roles', $roles);
+        return $this;
+    }
+
+    /**
+     * @param $role
+     * @return mixed
+     */
+    public function hasRole($role)
+    {
+        return in_array($role, $this->getRoles());
+    }
+
+    /**
+     * @param $roles
+     * @return mixed
+     */
+    public function hasRoles($roles)
+    {
+        $currentRoles = $this->getRoles();
+        foreach ($roles as $role) {
+            if (!in_array($role, $currentRoles)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles()
+    {
+        $roles = $this->getAttribute('roles');
+
+        if (is_null($roles)) {
+            $roles = [];
+        }
+
+        return $roles;
     }
 }
